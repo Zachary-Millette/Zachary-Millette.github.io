64c34f20d2a198b799ea904a38c38aba41af2fe5
Zach <zacharymillette@gmail.com>
2018-04-08 12:10:09 -0400

    Initial Commit
    Hopefully all images work now.

---
+++ js/gamesScript.js
@@ -0,0 +1,168 @@
+//JavaScript file for the Games page
+
+
+//Default slide index for each slide-show
+var pbSI = 0;
+var prSI = 0;
+var ntSI = 0;
+var aonSI = 0;
+
+
+//When this page loads the following will execute
+window.onload = function(){
+	//Initialize each slide-sshow by calling the respective function
+	showPBSlide(pbSI);
+	showPRSlide(prSI);
+	showNTSlide(ntSI);
+	showAONSlide(aonSI);
+}
+
+
+//Function for handling next and previous arrows
+function plusSlides(n, name){
+	if(name == 'pb'){
+		pbSI += n;
+		showPBSlide(pbSI);
+	}
+	else if(name == 'pr'){
+		prSI += n;
+		showPRSlide(prSI);
+	}
+	else if(name == 'nt'){
+		ntSI += n;
+		showNTSlide(ntSI);
+	}
+	else if(name == 'aon'){
+		aonSI += n;
+		showAONSlide(aonSI);
+	}
+}
+
+
+//Project Backfire Sldies
+function showPBSlide(n){
+	var i, pbSlides, pbThumbs;
+	
+	//Store all elements with the pbSlide class name
+	pbSlides = document.getElementsByClassName("pbSlide");
+	//Store all elements with the pbThumb class name
+	pbThumbs = document.getElementsByClassName("pbThumb");
+	
+	//Handle n becoming greater than the number of slides
+	if(n >= pbSlides.length){pbSI = 0;}
+	//Handle if n goes below 0
+	else if(n < 0){pbSI = pbSlides.length - 1;}
+	//if neither is the case
+	else{pbSI = n;}
+	
+	//Set all slides to display none
+	for(i = 0; i < pbSlides.length; i++){
+		pbSlides[i].style.display = "none";
+	}
+	
+	//Set the thumbs to not be the current slide
+	for(i = 0; i < pbThumbs.length; i++){
+		pbThumbs[i].className = pbThumbs[i].className.replace(" currentSlide", "");
+	}
+	
+	//Show the current images and add on to the thumb className
+	pbSlides[pbSI].style.display = "block";
+	pbThumbs[pbSI].className += " currentSlide";
+}
+
+
+//Project RedTide Slides
+function showPRSlide(n){
+	var i, prSlides, prThumbs;
+	
+	prSlides = document.getElementsByClassName('prSlide');
+	prThumbs = document.getElementsByClassName('prThumb');
+	
+	if(n >= prSlides.length){prSI = 0;}
+	else if(n < 0){prSI = prSlides.length - 1;}
+	else{prSI = n;}
+	
+	for (i = 0; i < prSlides.length; i++){
+		prSlides[i].style.display = "none";
+	}
+	
+	for(i = 0; i < prThumbs.length; i++){
+		prThumbs[i].className = prThumbs[i].className.replace(" currentSlide", "");
+	}
+	
+	prSlides[prSI].style.display = "block";
+	prThumbs[prSI].className += " currentSlide";
+	
+}
+
+
+//Night Terrors Slides
+function showNTSlide(n){
+	var i, ntSlides, ntThumbs;
+	
+	ntSlides = document.getElementsByClassName('ntSlide');
+	ntThumbs = document.getElementsByClassName('ntThumb');
+	
+	if(n >= ntSlides.length){ntSI = 0;}
+	else if(n < 0){ntSI = ntSlides.length - 1;}
+	else{ntSI = n;}
+	
+	for (i = 0; i < ntSlides.length; i++){
+		ntSlides[i].style.display = "none";
+	}
+	
+	for(i = 0; i < ntThumbs.length; i++){
+		ntThumbs[i].className = ntThumbs[i].className.replace(" currentSlide", "");
+	}
+	
+	ntSlides[ntSI].style.display = "block";
+	ntThumbs[ntSI].className += " currentSlide";
+	
+}
+
+
+//AON Slides
+function showAONSlide(n){
+	var i, aonSlides, aonThumbs;
+	
+	aonSlides = document.getElementsByClassName('aonSlide');
+	aonThumbs = document.getElementsByClassName('aonThumb');
+	
+	if(n >= aonSlides.length){aonSI = 0;}
+	else if(n < 0){aonSI = aonSlides.length -1;}
+	else{aonSI = n;}
+	
+	for (i = 0; i < aonSlides.length; i++){
+		aonSlides[i].style.display = "none";
+	}
+	
+	for(i = 0; i < aonThumbs.length; i++){
+		aonThumbs[i].className = aonThumbs[i].className.replace(" currentSlide", "");
+	}
+	
+	aonSlides[aonSI].style.display = "block";
+	aonThumbs[aonSI].className += " currentSlide";
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,33 @@
+//This is the JavaScript file for the Overlay
+
+
+//Global Variables
+/*	Canvas is triaged for the meantime
+var cnvas;
+var context;
+
+
+window.onload = function(){
+	canvas = document.getElementById('canvas');
+	context = canvas.getContext('2d');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context.fillStyle = '#404E4D';
+	context.fillRect(0,0, canvas.width, canvas.height);
+}
+*/
+
+//Function that is called when a menu selection is made
+function openTab(evt){
+	//All tabs are stored in an array
+	var overlayTabs;
+	overlayTabs = document.getElementsByClassName("overlayTab");
+	
+	//This function loops through each tab and ensures it is not active
+	for(i = 0; i < overlayTabs.length; i++){
+		overlayTabs[i].className = overlayTabs[i].className.replace(" active", ""); 
+	}
+	
+	//Set the tab that was selected to be active
+	evt.currentTarget.className += " active";
+}
